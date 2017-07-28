4334d913ef531d02f63677c256d35c88427216b5
Howard Shaw <chip.shaw4@gmail.com>
2017-07-27 23:02:13 -0600

    finished UI except for wins/losses and bad guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,145 @@
 
+
+//split by newline (\n) and replace each line starting from top... join with newlines
+// top line must be full length
+
+const blankScreen = 
+`<span id="l0">                                                                         </span>
+<span id="l1">                                                                         </span>
+<span id="l2">                                                                         </span>
+<span id="l3">                                                                         </span>
+<span id="l4">                                                                         </span>
+<span id="l5">                                                                         </span>
+<span id="l6">                                                                         </span>
+<span id="l7">                                                                         </span>
+<span id="l8">                                                                         </span>
+<span id="l9">                                                                         </span>
+<span id="l10">                                                                         </span>
+<span id="l11">                                                                         </span>
+<span id="l12">                                                                         </span>
+<span id="l13">                                                                         </span>
+<span id="l14">                                                                         </span>
+<span id="l15">                                                                         </span>
+<span id="l16">                                                                         </span>
+<span id="l17">                                                                         </span>
+<span id="l18">                                                                         </span>
+`
+const homeScreen = 
+`                                                                         
+
+
+                                                                         
+                                                                         
+                                                                         
+                     [++]=======================[++] 
+                      ||                         ||
+                      ||      H A N G M A N      ||
+                      ||                         ||
+                      ||  <span class="blinking-text">Press any key to play</span>  ||   
+                      ||                         ||
+                     [++]=======================[++]                  
+                                                                      
+                                                                       
+    
+                                                                         
+               
+
+`
+//ui screen is 19 characters high
+//ui screen is 73 characters wide
+
+// "You Win/Lose" statement should be 9 characters wide
+// body sections should be 5 characters wide
+
+//word should be maximum 14 characters
+
+var body = [
+" ,;, ",
+"<span class=\"blinking-eyes\"><span class=\"open\">('_')</span><span class=\"closed\">(-_-)</span></span>",
+"  |  ",
+" /M\\ ",
+"/ H \\",
+"  X  ",
+" / \\ ",
+"/   \\",
+"     ",
+] 
+
+body = {
+   empty: [
+   '<span id="body0">     </span>',
+   '<span id="body1">     </span>',
+   '<span id="body2">     </span>',
+   '<span id="body3">     </span>',
+   '<span id="body4">     </span>',
+   '<span id="body5">     </span>',
+   '<span id="body6">     </span>',
+   '<span id="body7">     </span>',
+   '<span id="body8">     </span>',
+   ],
+   
+   /*normal: [
+   " ,;, ",
+   "<span class=\"blinking-eyes\"><span class=\"open\">('_')</span><span class=\"closed\">(-_-)</span></span>",
+   "  |  ",
+   " /M\\ ",
+   "/ H \\",
+   "  X  ",
+   " / \\ ",
+   "/   \\",
+   "     ",
+   ],*/
+   hair: " ,;, ",
+   heads: {
+      normal: "<span class=\"blinking-eyes\"><span class=\"open\">('_')</span><span class=\"closed\">(-_-)</span></span>",
+      happy: "(^_^)",
+      sad: "(-,-)",
+   },
+   neck: "  |  ",
+   torso: {
+      upper: ["  M  ", " /M  ", " /M\\ "],
+      lower: ["  H  ", "/ H  ", "/ H \\"]
+   },
+   hips: "  X  ",
+   legs: {
+      upper: [" /   ", " / \\ "],
+      lower: ["/    ", "/   \\"]
+   }
+}
+//    (-_-)     ('_') > (o_o) > (-‸-) > (x‸x)    (-‸-) <sad when miss
+
+
+var winText = '<span class="blinking-text">You Win! </span>   Play Again? (y/n)';
+var loseText = '<span class="blinking-text">You Lose!</span>   Play Again? (y/n)';
+
+var endGameOK = '             OK.             '
+var endGame = '            Wait.            '
+
+const hangmanScreen = 
+`                                                                         
+
+                      <span id="end-game">${endGame}</span>                      
+  
+           
+          [++]=====[+]               
+           ||       |              
+           ||     ${body.empty[0]}        word: <span id="word"></span>
+           ||     ${body.empty[1]}        
+           ||     ${body.empty[2]}              
+           ||     ${body.empty[3]}        misses: <span id="misses"></span>
+           ||     ${body.empty[4]}                 
+           ||     ${body.empty[5]}        
+           ||     ${body.empty[6]}        wins: <span id="wins"></span> 
+          /++\\    ${body.empty[7]}        losses: <span id="losses"></span>
+         //||\\\\   ${body.empty[8]}                                                
+        // || \\\\                                                       
+  =====================================================================  
+
+`
+
+
+
+
 const wordList = ['Simpsons', 
                 'Looney Tunes', 
                 'South Park',
@@ -13,7 +154,11 @@ const wordList = ['Simpsons',
 //a callback function with an argument value that...
 // represents whether the game has been won 'hasWon' 
 
-function game(word, ui, wins, callback){
+function game(word, ui, wins, losses, callback){
+   //track wins and losses
+   this.wins = wins;
+   this.losses = losses;
+
    //when true, keypresses will guess letter 
    //is set false on guess
    //is set true after animations finish
@@ -49,8 +194,6 @@ function game(word, ui, wins, callback){
 
    this.callback = callback;
 
-   this.ui.initNewGame(this);
-
 
 
    console.log('game');
@@ -66,7 +209,7 @@ game.prototype.guess = function(letter) {
 
       //disable input until animations finish
       //and store gussed letter
-      `this.ready = false;`
+      this.ui.setWait(this);
       this.guessedLetters.push(letter);
 
       //find all indices of guessed letter and push...
@@ -79,17 +222,19 @@ game.prototype.guess = function(letter) {
       }
 
       //if letter is found in our word,
-      //update word as seen by user and
+      //update word as seen by user 
       if( indices.length ){
          indices.forEach((i) => {
             this.userArr[i] = this.word[i];
          });
+         var goodGuess = true;
       }
       //otherwise reduce guesses left by one,
       //push letter to misses, and update hangman,
       else{
          this.guessesLeft -= 1;
          this.misses.push(letter);
+         goodGuess = false;
       }
 
 
@@ -100,16 +245,23 @@ game.prototype.guess = function(letter) {
       if(this.userArr.join('') === this.word){
          this.isFinished = true;
          this.win = true;
+         this.wins += 1;
          console.log('winnar');
       } 
       //check for loss
       else if( this.guessesLeft === 0){
          this.isFinished = true;
          this.win = false;
+         this.losses += 1;
          console.log('loser');
       }
 
-      //this.ui.update(letter, wasinword??, this);
+      if( goodGuess ){
+         this.ui.goodChoice(this);
+      }
+      else{
+         this.ui.badChoice(this);
+      }
    }
 };
 
@@ -119,14 +271,150 @@ game.prototype.playAgain = function(answer) {
    this.callback(answer, this.win);
 };
 
-
-function ui(){
+//updates visual user interface
+//element arg is the DOM element to use for UI
+function ui(element, callback){
+   element.innerHTML = blankScreen;
+   this.homeScreen = homeScreen.split('\n');
+   this.hangmanScreen = hangmanScreen.split('\n');
+   setTimeout( this.changeScreen.bind(this, 0, this.homeScreen, callback, 150));
+   element.innerHTML = blankScreen;
+   console.log(element);
    console.log('ui');
 }
-ui.prototype.initNewGame = function(game) {
-   // delete later, this should happen after animations
-   game.ready = true;
-};
+//line = current line being set,
+//screenArr is the array of lines for new screen
+ui.prototype.changeScreen = function(line, screenArr, callback){
+   if (line > 18 ){
+      callback();
+   }
+   else{
+      document.getElementById(`l${line}`).innerHTML = screenArr[line];
+      setTimeout(this.changeScreen.bind(this, line + 1, screenArr, callback), 75);
+   }
+}
+ui.prototype.initGame = function(game){
+   var a = () => {
+      this.updateWins(game);
+      setTimeout(b, 500);
+   }
+   var b = () => {
+      this.updateWord(game);
+      setTimeout(c, 500);
+   }
+   var c = () => {
+      this.setReady(game);
+   }
+   a();
+}
+ui.prototype.updateWord = function(game){
+   document.getElementById('word').innerHTML = game.userArr.join(' ');
+}
+ui.prototype.updateMisses = function(game){
+   document.getElementById('misses').innerHTML = game.misses.join(', ');
+}
+//updates both wins and losses
+ui.prototype.updateWins = function(game){
+   document.getElementById('wins').innerHTML = game.wins;
+   document.getElementById('losses').innerHTML = game.losses;
+}
+//updates body parts
+ui.prototype.updateBody = function(partNum, html){
+   document.getElementById(`body${partNum}`).innerHTML = html;
+}
+//sets interface ready for input 
+ui.prototype.setReady = function(game){
+   document.getElementById('end-game').innerHTML = endGameOK;
+   game.ready = true; 
+}
+//sets interface to not accept user input
+ui.prototype.setWait = function(game){
+   document.getElementById('end-game').innerHTML = endGame;
+   game.ready = false; 
+}
+ui.prototype.setWin = function(game){
+   document.getElementById('end-game').innerHTML = winText;
+   game.ready = true; 
+}
+ui.prototype.setLoss = function(game){
+   document.getElementById('end-game').innerHTML = loseText;
+   game.ready = true; 
+}
+//update interface when user chose right letter
+ui.prototype.goodChoice = function(game){
+   //happy face (if guesses left is <= 5,
+   //update word,
+   //normal face, (if guesses left is <= 5),
+   //back to ready
+   var ready = () => {
+      if (game.guessesLeft <=5){
+         this.updateBody(1, body.heads.normal);
+      }
+      if( game.isFinished ){
+         //animate more
+         //change guy to dance 
+         this.updateWins(game);
+         this.setWin(game);
+      }
+      else{
+         this.setReady(game);
+      }
+   }
+   //iff guesses left is 6 just update word then ready
+   var word = () => {
+      this.updateWord(game);
+      setTimeout(ready, 500);
+   }
+   if (game.guessesLeft <= 5){
+      this.updateBody(1, body.heads.happy);
+   }
+   setTimeout(word, 500);
+}
+//update interface when user chose wrong letter
+ui.prototype.badChoice = function(game){
+   //sad face (if guesses left is < 5,
+   //update misses,
+   //normal face, (if guesses left is < 5),
+   //update limb,
+   //back to ready,
+   this.updateMisses(game);
+   this.setReady(game);
+   switch(game.guessesLeft){
+      case 5:
+         this.updateBody(0, body.hair);
+         this.updateBody(1, body.heads.normal);
+         break;
+      case 4:
+         this.updateBody(2, body.neck);
+         this.updateBody(3, body.torso.upper[0]);
+         this.updateBody(4, body.torso.lower[0]);
+         this.updateBody(5, body.hips);
+         break;
+      case 3:
+         this.updateBody(3, body.torso.upper[1]);
+         this.updateBody(4, body.torso.lower[1]);
+         break;
+      case 2:
+         this.updateBody(3, body.torso.upper[2]);
+         this.updateBody(4, body.torso.lower[2]);
+         break;
+      case 1:
+         this.updateBody(6, body.legs.upper[0]);
+         this.updateBody(7, body.legs.lower[0]);
+         break;
+      case 0:
+         this.updateBody(6, body.legs.upper[1]);
+         this.updateBody(7, body.legs.lower[1]);
+         break;
+   }
+   if( game.isFinished ){
+      this.updateWins(game);
+      this.setLoss(game);
+   }
+}
+
+//updatebody
+//finish game
 
 
 const hangman = {
@@ -137,32 +425,39 @@ const hangman = {
    //copy words array, so it can be reset
    words: wordList.slice(0),
    wins: 0,
+   losses: 0,
 
    //stores current active game
    //set null after game over (before new game)
    currentGame: null,
 
    //user interface object
-   ui: new ui(),
 
    //callback from game when game is over
    //prepares this object for a new game
    gameFinished: function(playAgain, hasWon) {
-      //track wins
+      //track wins/losses
       if( hasWon ){
          this.wins += 1;
+      }
+      else{
+         this.losses += 1;
       }
       //start a new game if they want to play again
       if(playAgain){
          this.startNewGame();
       }
       //back to homescreen if they dont want to play again
+      //reset data
       else{
+         callback = function(){this.ready = true }.bind(this);
          this.ready = false;
          this.currentGame = null;
          this.wins = 0;
+         this.losses = 0;
          this.words = wordList.slice(0);
          //update ui to home 
+         this.ui.changeScreen.bind(this.ui, 0, this.ui.homeScreen, callback, 150)()
       }
    },
 
@@ -183,10 +478,16 @@ const hangman = {
       this.currentGame = new game(word, 
                                   this.ui, 
                                   this.wins,
+                                  this.losses,
                                   this.gameFinished.bind(this)
                                  );
+      this.ui.changeScreen(0, 
+                           this.ui.hangmanScreen, 
+                           this.ui.initGame.bind(this.ui, this.currentGame)
+                          );
    }
 }
+hangman.ui = new ui(document.getElementById('ui'), () => {hangman.ready = true });
 
 //returns random integer, inclusive 
 function randint(min, max) {
@@ -208,7 +509,7 @@ function keyHandler(e){
    }
    //if current game has finished, y/n keys will...
    //either start a new game or go back to home screen
-   else if(game.isFinished){
+   else if(game.isFinished && game.ready){
       if( keyCode === 89){
          game.playAgain(true);
       }
@@ -232,22 +533,30 @@ window.addEventListener('keyup', keyHandler);
 
 
 
-//remove later just for dev
-function go(){
-   hangman.ready = true;
-}
 
-//    ,,,      ,,,       ,,,     ,,,     ,,,     ,,,
-//   (^_^)    (-_-)     ('_') > (o_o) > (-‸-) > (x‸x)   
+
+
+
+
+
+
+
+
+
+
+
+
+//    ,,,      ,,,       ,,,     ,,,     ,,,     ,,,      ,,,
+//   (^_^)    (-_-)     ('_') > (o_o) > (-‸-) > (x‸x)    (-‸-) <sad when miss
 //
 //
 //           (^_^)
-//            /|\_
+//           \/|\_
+//               
+//   regular eyes could be spans with before/after content that animates
 //
 //
-//
-//
-//
+//  blinking text can be done with spans and animation. 
 //
 //
 //
@@ -302,7 +611,7 @@ function go(){
 //          ||       X              misses: a, d, f, 
 //         /++\     | |              b, g, h, r, y, u, i,
 //        //||\\    | |
-//       // || \\                   wins: 0
+//       // || \\                   wins: 0        losses: 
 // =============================================================
 //
 //
